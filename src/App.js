0ce9f3ec05cc1a3e7c3da7d82a4c7db45e0ab900
Alex Martini <alexmartini.sc@gmail.com>
2023-04-06 14:05:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [lista, setLista] = useState([]);
+  let [novoItem, setNovoItem] = useState("");
+
+  useEffect(() => {
+    setLista(['Tomar banho']);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container border border-1 border-black shadow">
+      <h3>To-do List</h3>
+      <hr/>
+      <div className="new-item">
+        <input className="form-control shadow-sm" id="nomeItem" value={novoItem} onChange={value => setNovoItem(value.target.value)} type="text" placeholder="Nome da tarefa"></input>
+        <button className="btn btn-outline-info shadow-sm ms-3" onClick={() => adicionarItem()}>Adicionar</button>
+      </div>
+      <ul className="todo-list text-muted fw-bold">
+          {lista.map((item, index) => (
+            <li key={index} className="todo-item border-bottom">
+              {item}
+              <button className="btn btn-outline-danger shadow-sm" onClick={() => deletarItem(index)}>
+                <i class="fa-solid fa-trash-can"></i>
+              </button>
+            </li>
+          ))}
+      </ul>
     </div>
   );
+
+  function adicionarItem() {
+    if (novoItem.length <= 0) {
+      alert("Por favor, digite algo no campo!");
+      return;
+    }
+
+    let itemIndex = lista.indexOf(novoItem);
+    if (itemIndex >= 0) {
+      alert("Tarefa já existente.");
+      return;
+    }
+    
+    setLista([...lista, novoItem]);
+    alert("Tarefa adicionada com sucesso!");
+    setNovoItem("");
+  }
+
+  function deletarItem(index) {
+    let tmpArray = [...lista];
+    tmpArray.splice(index, 1);
+
+    setLista(tmpArray);
+  }
 }
+
+
 
 export default App;
